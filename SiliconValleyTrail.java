38c9bfd404f6ab9629afc63276f259a6209e4c1b
Ranjana Bongale Ganesh <ranjanabg.ranj@gmail.com>
2026-04-03 13:54:07 -0700

    Add main menu with welcome message and game options

--- SiliconValleyTrail.java
+++ SiliconValleyTrail.java
@@ -1,5 +1,12 @@
 public class SiliconValleyTrail {
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		System.out.println("========================================");
+		System.out.println("       Welcome to Silicon Valley Trail  ");
+		System.out.println("========================================");
+		System.out.println();
+		System.out.println("  1. New Game");
+		System.out.println("  2. Load Game");
+		System.out.println("  3. Quit");
+		System.out.println();
 	}
 }
